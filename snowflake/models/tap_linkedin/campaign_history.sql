ed1c7e54224cfa8a46c6d82a4e6115dd88dcfe4c
Dhrruv <dhrruv@Dhrruvs-MBP.bbrouter>
2023-04-12 17:36:53 +0530

    updated campaign dbt model

--- snowflake/models/tap_linkedin/campaign_history.sql
+++ snowflake/models/tap_linkedin/campaign_history.sql
@@ -4,5 +4,60 @@
    )
 }}
 
-SELECT *
+{% set json_column_query %}
+select distinct json.key as column_name
+
+FROM {{ source('tap_linkedin', 'campaign')}},
+
+lateral flatten(input=>DAILYBUDGET) json
+{% endset %}
+ 
+{% set daily_budget_results = run_query(json_column_query) %}
+
+{% if execute %}
+{# Return the first column #}
+{% set daily_budget_list = daily_budget_results.columns[0].values() %}
+{% else %}
+{% set daily_budget_list = [] %}
+{% endif %}
+
+SELECT ACCOUNT,
+       ACCOUNT_ID,
+       ASSOCIATEDENTITY,
+       ASSOCIATED_ENTITY_ORGANIZATION_ID,
+       ASSOCIATED_ENTITY_PERSON_ID,
+       AUDIENCEEXPANSIONENABLED,
+       CAMPAIGNGROUP,
+       CAMPAIGN_GROUP_ID,
+       CHANGEAUDITSTAMPS,
+       COSTTYPE,
+       CREATIVESELECTION,
+       DAILYBUDGET,
+       FORMAT,
+       ID,
+       LOCALE,
+       NAME,
+       OBJECTIVETYPE,
+       OFFSITEDELIVERYENABLED,
+       OFFSITEPREFERENCES,
+       OPTIMIZATIONTARGETTYPE,
+       PACINGSTRATEGY,
+       RUNSCHEDULE,
+       SERVINGSTATUSES,
+       STATUS,
+       STORYDELIVERYENABLED,
+       TARGETING,
+       TARGETINGCRITERIA,
+       TEST,
+       TOTALBUDGET,
+       TYPE,
+       UNITCOST,
+       VERSION,
+       VERSION_TAG,
+
+
+{% for column_name in daily_budget_list %}
+DAILYBUDGET:{{column_name}}::varchar as {{column_name}}{%- if not loop.last %},{% endif -%}
+{% endfor %}       
+
 FROM {{ source('tap_linkedin', 'campaign') }} as campaign_history
