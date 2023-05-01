3f3867e1607f19977a052e0bc77459fc62dd758f
NeilGorman104 <neil@ryan-miranda.com>
2023-05-01 15:43:10 -0400

    Remove extra unused From (Select) statement

--- snowflake/models/tap_linkedin/campaign_group_history.sql
+++ snowflake/models/tap_linkedin/campaign_group_history.sql
@@ -12,18 +12,7 @@ SELECT ID,
        STATUS,
        RUN_SCHEDULE_START,
        RUN_SCHEDULE_END,
-       ACCOUNT_ID
-FROM (SELECT ID,
-       LAST_MODIFIED_TIME,
-       CREATED_TIME,
-       NAME,
-       BACKFILLED,
-       STATUS,
-
-       RUN_SCHEDULE_START,
-       RUN_SCHEDULE_END,
-
        ACCOUNT_ID,
        _SDC_BATCHED_AT
 
-FROM {{ source('tap_linkedin', 'campaign_groups') }} as campaign_group_history)
+FROM {{ source('tap_linkedin', 'campaign_groups') }} as campaign_group_history
